40a501cc5792ec9f3529438e8f8152c6550ef9c5
Humaedi <Humaedi.medi99@gmail.com>
2022-11-08 14:13:57 +0700

    add bookshelf & check node_env

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Submission_Bookshelf_API_dicoding
+
+``` npm run start ``` untuk menjalankan
+``` $env:NODE_ENV="Development" ``` Untuk mengganti mode dev ke prod atau sebaliknya.
--- resource/server.js
+++ resource/server.js
@@ -1,14 +1,14 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
+const environment = process.env.NODE_ENV;
+
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: 'localhost',
     routes: {
-      cors: {
-        origin: ['*'],
-      },
+      cors: true
     },
   });
 
@@ -16,6 +16,7 @@ const init = async () => {
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
+  console.log(`Mode environment: ${environment}`)
 };
 
 init();
