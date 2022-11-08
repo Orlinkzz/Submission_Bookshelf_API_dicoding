dc4893db96d0cd218598e54de32f3d48cb2a0f66
Humaedi <Humaedi.medi99@gmail.com>
2022-11-08 13:54:46 +0700

    add bookshelf

---
+++ src/books.js
@@ -0,0 +1,32 @@
+const books = [
+  {
+    id: 'I9nbyZeTUmpGEo69',
+    name: 'Buku A',
+    year: 2020,
+    author: "Penulis A",
+    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    publisher: "Penerbit A",
+    pageCount: 100,
+    readPage: 10,
+    finished: false,
+    reading: false,
+    insertedAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  },
+  {
+    id: 'I9nbyZeTUmpHUio9',
+    name: 'Buku B',
+    year: 2020,
+    Author: "Penulis B",
+    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    publisher: "Penerbit B",
+    pageCount: 100,
+    readPage: 10,
+    finished: false,
+    reading: false,
+    insertedAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  },
+];
+
+module.exports = books;
